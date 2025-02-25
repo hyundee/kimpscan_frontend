caf7488dc4cf75e841ef99f268450e4a17075437
hyundee <hyundee9@gmail.com>
2025-02-25 17:48:17 +0900

    feat : 하단 아이콘 설정

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-// import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeScreen} from '../screens/HomeScreen';
 import {MyPageScreen} from '../screens/MyPageScreen';
 import {Header} from '../components/Header/Header';
-
+import Home from 'react-native-vector-icons/Foundation';
+import User from 'react-native-vector-icons/Feather';
 // const Stack = createStackNavigator();
 
 const Tab = createBottomTabNavigator();
@@ -20,12 +20,22 @@ export const AppNavigator = () => {
         <Tab.Screen
           name="Home"
           component={HomeScreen}
-          options={{title: '홈'}}
+          options={{
+            title: '',
+            tabBarIcon: ({color, size}) => (
+              <Home name="home" size={size} color={color} />
+            ),
+          }}
         />
         <Tab.Screen
           name="MyPage"
           component={MyPageScreen}
-          options={{title: '내 정보'}}
+          options={{
+            title: '',
+            tabBarIcon: ({color, size}) => (
+              <User name="user" size={size} color={color} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
