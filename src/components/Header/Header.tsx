33d42aaa92bee6867c720a0504c92eda13d10d8e
hyundee <hyundee9@gmail.com>
2025-03-26 12:07:58 +0900

    feat : header 로고 적용

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, StyleSheet, Image} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useNavigation, NavigationProp} from '@react-navigation/native';
 import {RootStackParamList} from '../../types/navigation';
@@ -22,14 +22,17 @@ export const Header = () => {
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <TouchableOpacity onPress={() => navigation.navigate('Home')}>
-          <Text style={styles.logo}>LOGO</Text>
+          {/* <Text style={styles.logo}>LOGO111</Text> */}
+          <Image
+            source={require('../../assets/images/logo_with_letter.png')}
+            style={styles.logo}
+          />
         </TouchableOpacity>
         <View style={styles.buttonContainer}>
           <SearchBar />
           <TouchableOpacity
             onPress={() => navigation.navigate('Notifications')}>
             <NotificationsIcon name="notifications" size={25} color="#000" />
-            {/* <Text style={styles.logo}>알람</Text> */}
           </TouchableOpacity>
         </View>
       </View>
@@ -51,9 +54,9 @@ const styles = StyleSheet.create({
     paddingHorizontal: 15,
   },
   logo: {
-    color: '#000',
-    fontSize: 18,
-    // fontWeight: 'bold',
+    width: 130,
+    height: '100%',
+    resizeMode: 'contain',
   },
   buttonContainer: {
     flexDirection: 'row',
