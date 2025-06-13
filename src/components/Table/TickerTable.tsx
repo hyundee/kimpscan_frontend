5e01e91f6a13f5836f36aee4d5b622d1be3b989a
a3magic3pocket <a3magic3pocket@gmail.com>
2025-06-14 00:02:01 +0900

    feat: 티커 테이블에서 변경된 데이터만 리렌더링 하도록 수정.

--- src/components/Table/TickerTable.tsx
+++ src/components/Table/TickerTable.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 import {
-  FlatList,
   ScrollView,
   StyleSheet,
   Text,
@@ -21,15 +20,19 @@ import { loadBookmarks, saveBookmarks } from '../../utils/bookmarkStorage';
 import { ChartLegend } from '../Legend/ChartLegend';
 import { useSelectedCoin } from '../../store/useSelectedCoin';
 import { SearchBar } from '../SearchBar/SearchBar';
+import { crc32 } from '../../utils/crc';
+import { ITableDataRow } from '../../types/CoinTable';
+import CellRenderer from './CellRenderer';
 
 interface ITickerTable {
   data: Record<string, CoinInfo>;
+  isDiffCoin: boolean;
 }
 
-export const TickerTable = ({ data }: ITickerTable) => {
+export const TickerTable = ({ data, isDiffCoin }: ITickerTable) => {
+  const [initCoinList, setInitCoinList] = useState<Record<string, CoinInfo>>({});
   const [coinList, setCoinList] = useState<Record<string, CoinInfo>>({});
   const [query, setQuery] = useState('');
-  const [filteredData, setFilteredData] = useState<CoinInfo[]>([]);
   const [bookMarks, setBookMarks] = useState<Record<string, boolean>>({});
   const kimpHistoryRef = useRef<Record<string, number>>({});
 
@@ -37,11 +40,100 @@ export const TickerTable = ({ data }: ITickerTable) => {
     null,
   );
   const [sortOrder, setSortOrder] = useState<'asc' | 'desc'>('asc');
-  const [tableData, setTableData] = useState<(React.JSX.Element | (string | undefined)[] | (string | React.JSX.Element)[])[][]>();
+  const [tableData, setTableData] = useState<ITableDataRow[]>([]);
 
   useEffect(() => {
-    setCoinList(data);
-  }, [data]);
+    // 최초 데이터 갱신
+    if (!isDiffCoin) {
+      setInitCoinList(data);
+      setCoinList(data);
+      setTableData(
+        Object.values(data).map(value => getTableDataRow(value))
+      )
+    }
+  }, [data, isDiffCoin]);
+
+  useEffect(() => {
+    // 데이터 갱신
+    if (isDiffCoin) {
+      const updatedCoinList: Record<string, CoinInfo> = Object.keys(initCoinList).reduce(
+        (acc, symbol) => {
+          const original = initCoinList[symbol];
+          const updates = data[symbol];
+
+          acc[symbol] = updates
+            ? { ...original, ...updates }
+            : original;
+
+          return acc;
+        },
+        {} as Record<string, CoinInfo>
+      );
+      setCoinList(updatedCoinList);
+    }
+
+  }, [initCoinList, data, isDiffCoin])
+
+  useEffect(() => {
+    // 필터
+    const lowerCaseQuery = query.toLowerCase()
+    const filteredData = Object.values(coinList).filter(item => {
+      const korName = item.korName?.toLowerCase() || '';
+      const rootSymbol = item.rootSymbol?.toLowerCase() || '';
+      return korName.includes(lowerCaseQuery) || rootSymbol.includes(lowerCaseQuery);
+    })
+
+    // 정렬
+    // filteredData.sort((a, b) => {
+    //   let aValue: number | string = '';
+    //   let bValue: number | string = '';
+    //   if (sortKey === 'korName') {
+    //     aValue = a.korName!;
+    //     bValue = b.korName!;
+    //   } else if (sortKey === 'kimp') {
+    //     aValue = Number(a.kimp);
+    //     bValue = Number(b.kimp);
+    //   } else if (sortKey === 'volume') {
+    //     aValue = Number(a.won24hVolume) + Number(a.usdt24hVolume);
+    //     bValue = Number(b.won24hVolume) + Number(b.usdt24hVolume);
+    //   }
+    //   if (aValue < bValue) {
+    //     return sortOrder === 'asc' ? -1 : 1;
+    //   }
+    //   if (aValue > bValue) {
+    //     return sortOrder === 'asc' ? 1 : -1;
+    //   }
+    //   return 0;
+    // });
+
+    // 북마크
+    console.log('filteredData', filteredData);
+
+    setTableData(prevTableData => {
+      const newTableData: ITableDataRow[] = [];
+      console.log('prevTableData', prevTableData);
+
+      for (const value of prevTableData) {
+        const symbol = `${value.symbol[0]}USDT`
+        if (symbol in coinList) {
+          const newValue = coinList[symbol]
+          newTableData.push(getTableDataRow(newValue))
+        } else {
+          newTableData.push(value)
+        }
+      }
+
+      return newTableData;
+    });
+
+
+    console.log("coinList", coinList)
+    console.log('sortKey', sortKey);
+    console.log('sortOrder', sortOrder);
+    console.log('bookMarks', bookMarks);
+    console.log('query', query);
+    console.log('',);
+  }, [coinList, sortKey, sortOrder, query])
 
   useEffect(() => {
     Object.entries(coinList).forEach(([symbol, info]) => {
@@ -53,6 +145,45 @@ export const TickerTable = ({ data }: ITickerTable) => {
   useEffect(() => {
     loadBookmarks().then(setBookMarks);
   }, []);
+
+  const getTableDataRow = (value: CoinInfo) => {
+    const coinSymbol = value.rootSymbol;
+    const wonPrice = formatPrice(value.wonPrice);
+    const usdtPrice = formatInteger(value.usdtPrice);
+    const finalUsdtPrice =
+      usdtPrice < 10 ? Number(value.usdtPrice).toFixed(4) : usdtPrice;
+
+    const currentKimp = Number(value.kimp);
+    const previousKimp = kimpHistoryRef.current[coinSymbol!];
+    const kimpColor =
+      previousKimp !== undefined && currentKimp > Number(previousKimp)
+        ? 'red'
+        : 'blue';
+
+    kimpHistoryRef.current[coinSymbol!] = currentKimp;
+
+    const kimpValue = Number(value.kimp).toFixed(3);
+    const kimpPriceText = `${wonPrice}/${finalUsdtPrice}`;
+
+    const won24hVolume = formatInteger(value.won24hVolume);
+    const usdt24hVolume = formatInteger(value.usdt24hVolume);
+    const volumeRatio = `${Math.floor((won24hVolume / usdt24hVolume) * 100)}%`;
+    const volumeRatioText = `${formatNumber(won24hVolume)}/${formatNumber(
+      usdt24hVolume,
+    )}`;
+
+    // console.log('coinSymbol', coinSymbol);
+    // console.log('value.korName', value.korName);
+
+    const tableDataRow: ITableDataRow = {
+      rowHash: getCoinInfoHash(value),
+      symbol: [coinSymbol ?? "", value.korName ?? ""],
+      kimpPrice: [kimpColor, kimpValue, kimpPriceText],
+      volumeRatio: [volumeRatio, volumeRatioText],
+    }
+
+    return tableDataRow;
+  }
 
   const toggleBookMark = (symbol?: string) => {
     if (symbol) {
@@ -67,6 +198,14 @@ export const TickerTable = ({ data }: ITickerTable) => {
     }
   };
 
+  const getCoinInfoHash = (info: CoinInfo) => {
+    const str = Object.values(info ?? {})
+      .map(value => value ?? '')
+      .join('|');
+
+    return crc32(str).toString(16).padStart(8, '0');
+  }
+
   const handleSort = (key: 'korName' | 'kimp' | 'volume') => {
     if (sortKey === key) {
       setSortOrder(prev => (prev === 'asc' ? 'desc' : 'asc'));
@@ -76,118 +215,8 @@ export const TickerTable = ({ data }: ITickerTable) => {
     }
   };
 
-  useEffect(() => {
-    const baseData = query ? filteredData : Object.values(coinList);
-
-    const bookmarkedFirst = baseData.sort((a, b) => {
-      const aBookmarked = bookMarks[a.rootSymbol!] ?? false;
-      const bBookmarked = bookMarks[b.rootSymbol!] ?? false;
-      if (aBookmarked === bBookmarked) {
-        return 0;
-      }
-      return aBookmarked ? -1 : 1;
-    });
-
-    if (sortKey) {
-      bookmarkedFirst.sort((a, b) => {
-        let aValue: number | string = '';
-        let bValue: number | string = '';
-        if (sortKey === 'korName') {
-          aValue = a.korName!;
-          bValue = b.korName!;
-        } else if (sortKey === 'kimp') {
-          aValue = Number(a.kimp);
-          bValue = Number(b.kimp);
-        } else if (sortKey === 'volume') {
-          aValue = a.won24hVolume! + a.usdt24hVolume;
-          bValue = b.won24hVolume! + b.usdt24hVolume;
-        }
-        if (aValue < bValue) {
-          return sortOrder === 'asc' ? -1 : 1;
-        }
-        if (aValue > bValue) {
-          return sortOrder === 'asc' ? 1 : -1;
-        }
-        return 0;
-      });
-    }
-
-    const sortedCoinList = bookmarkedFirst;
-
-    const newTableData =
-      sortedCoinList.map(value => {
-        const coinSymbol = value.rootSymbol;
-        const wonPrice = formatPrice(value.wonPrice);
-        const usdtPrice = formatInteger(value.usdtPrice);
-        const finalUsdtPrice =
-          usdtPrice < 10 ? Number(value.usdtPrice).toFixed(4) : usdtPrice;
-
-        const currentKimp = Number(value.kimp);
-        const previousKimp = kimpHistoryRef.current[coinSymbol!];
-        const kimpColor =
-          previousKimp !== undefined && currentKimp > Number(previousKimp)
-            ? 'red'
-            : 'blue';
-
-        kimpHistoryRef.current[coinSymbol!] = currentKimp;
-
-        const kimpValue = Number(value.kimp).toFixed(3);
-        const finalKimpPrice = <Text style={{ color: kimpColor }}>{kimpValue}%</Text>;
-        const kimpPriceText = `${wonPrice}/${finalUsdtPrice}`;
-
-        const won24hVolume = formatInteger(value.won24hVolume);
-        const usdt24hVolume = formatInteger(value.usdt24hVolume);
-        const volumeRatio = `${Math.floor((won24hVolume / usdt24hVolume) * 100)}%`;
-        const volumeRatioText = `${formatNumber(won24hVolume)}/${formatNumber(
-          usdt24hVolume,
-        )}`;
-
-        const bookMarkButton = (
-          <TouchableOpacity
-            style={styles.bookmarkContainer}
-            onPress={() => toggleBookMark(coinSymbol)}>
-            <Icon
-              name={bookMarks[coinSymbol!] ? 'star' : 'staro'}
-              size={20}
-              color="#000"
-            />
-          </TouchableOpacity>
-        );
-
-        return [
-          [coinSymbol, value.korName],
-          [finalKimpPrice, kimpPriceText],
-          [volumeRatio, volumeRatioText],
-          bookMarkButton,
-        ];
-      });
-    setTableData(newTableData);
-  }, [query, filteredData, coinList, bookMarks])
-
   const headerTitles = ['종목명', '김프', '거래비율', '즐겨\n찾기'];
   const sortKeys = ['korName', 'kimp', 'volume', 'none'];
-
-  const renderCustomCell = (
-    cellData:
-      | React.JSX.Element
-      | (string | React.JSX.Element)[]
-      | (string | undefined)[],
-  ) => {
-    if (Array.isArray(cellData)) {
-      return (
-        <View style={styles.cellContainer}>
-          <Text style={styles.strongText}>{cellData[0]}</Text>
-          <Text style={styles.smallText}>{cellData[1]}</Text>
-        </View>
-      );
-    }
-
-    if (React.isValidElement(cellData) || typeof cellData === 'string') {
-      return cellData;
-    }
-
-    return <Text>-</Text>; // 예외 처리
-  };
 
   const handlePress = (symbol: string[]) => {
     const coinKey = symbol[0] + 'USDT';
@@ -204,7 +233,6 @@ export const TickerTable = ({ data }: ITickerTable) => {
         item.rootSymbol!.toLowerCase().includes(text.toLowerCase())
       );
     });
-    setFilteredData(result);
   };
 
   // console.log(filteredData);
@@ -245,34 +273,49 @@ export const TickerTable = ({ data }: ITickerTable) => {
             textStyle={styles.headerText}
             flexArr={flexArr}
           />
-          <FlatList
-            data={tableData}
-            keyExtractor={(_, index) => index.toString()}
-            renderItem={({ item: rowData, index: rowIndex }) => (
-              <TableWrapper key={rowIndex} style={styles.row}>
-                {rowData.map((cellData, cellIndex) => (
-                  <Cell
-                    key={cellIndex}
-                    data={
-                      cellIndex === 0 ? (
-                        <TouchableOpacity
-                          onPress={() => handlePress(rowData[0] as string[])}>
-                          {renderCustomCell(cellData)}
-                        </TouchableOpacity>
-                      ) : (
-                        renderCustomCell(cellData)
-                      )
-                    }
-                    style={{ flex: flexArr[cellIndex] }}
-                  />
-                ))}
-              </TableWrapper>
-            )}
-            initialNumToRender={20}
-            maxToRenderPerBatch={10}
-            windowSize={21}
-            scrollEnabled={false}
-          />
+          {tableData.map((item) => (
+            <TableWrapper key={`${item.rowHash}-wrap`} style={styles.row}>
+              <Cell
+                key={`${item.rowHash}-0`}
+                data={
+                  <TouchableOpacity
+                    onPress={() => handlePress(item.symbol)}>
+                    <CellRenderer cellData={item.symbol} cellDataType={"symbol"} />
+                  </TouchableOpacity>
+                }
+                style={{ flex: flexArr[0] }}
+              />
+              <Cell
+                key={`${item.rowHash}-1`}
+                data={
+                  <CellRenderer cellData={item.kimpPrice} cellDataType={"kimpPrice"} />
+                }
+                style={{ flex: flexArr[1] }}
+              />
+              <Cell
+                key={`${item.rowHash}-2`}
+                data={
+                  <CellRenderer cellData={item.volumeRatio} cellDataType={"volumeRatio"} />
+                }
+                style={{ flex: flexArr[2] }}
+              />
+              <Cell
+                key={`${item.rowHash}-3`}
+                data={
+                  <TouchableOpacity
+                    style={styles.bookmarkContainer}
+                    onPress={() => toggleBookMark(item.symbol[0])}>
+                    <Icon
+                      name={bookMarks[item.symbol[0]] ? 'star' : 'staro'}
+                      size={20}
+                      color="#000"
+                    />
+                  </TouchableOpacity>
+                }
+                style={{ flex: flexArr[3] }}
+              />
+            </TableWrapper>
+          ))}
         </Table>
       </ScrollView>
     </View>
@@ -323,22 +366,6 @@ const styles = StyleSheet.create({
   row: {
     flexDirection: 'row',
   },
-  cellContainer: {
-    margin: 3,
-    justifyContent: 'center',
-    gap: 2,
-  },
-  strongText: {
-    fontWeight: 'bold',
-    fontSize: 15,
-  },
-  smallText: {
-    fontSize: 12,
-  },
-  bookmarkContainer: {
-    width: '100%',
-    alignItems: 'center',
-  },
   bookmark: {
     // width: '100%',
     // justifyContent: 'center',
@@ -347,5 +374,9 @@ const styles = StyleSheet.create({
   },
   sort: {
     marginLeft: 3,
+  },
+  bookmarkContainer: {
+    width: '100%',
+    alignItems: 'center',
   },
 });
