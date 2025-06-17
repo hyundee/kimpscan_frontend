6c9084e3667c4911f360e3858b40ca47f96df38d
hyundee <hyundee9@gmail.com>
2025-06-17 21:03:08 +0900

    feat : 내정보페이지(알람) 구현

--- src/components/Notification/NotificationModal.tsx
+++ src/components/Notification/NotificationModal.tsx
@@ -1,38 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text, StyleSheet, TextInput} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
 export const NotificationModal = () => {
+  const [selectedValue, setSelectedValue] = useState('BTC');
+
   return (
-    <View style={styles.modal}>
-      <Text>종목</Text>
-      <Text>김치프리미엄</Text>
-      <TextInput>5.8%</TextInput>
-      <Text>동일 알람 방지 기간</Text>
-      <TextInput>3600분</TextInput>
+    <View style={styles.container}>
+      <Text style={styles.text}>종목</Text>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={itemValue => setSelectedValue(itemValue)}
+        style={styles.input}>
+        <Picker.Item label="BTC" value="BTC" />
+        <Picker.Item label="ETH" value="ETH" />
+        <Picker.Item label="SOL" value="SOL" />
+        <Picker.Item label="AXL" value="AXL" />
+      </Picker>
+      <Text style={styles.text}>김치프리미엄</Text>
+      <TextInput style={styles.input}>5.8%</TextInput>
+      <Text style={styles.text}>동일 알람 방지 기간</Text>
+      <TextInput style={styles.input}>3600분</TextInput>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    // width: '100%',
-    padding: 20,
-    flex: 1,
-    justifyContent: 'flex-start',
-    // alignItems: 'flex-start',
+    padding: 30,
+    backgroundColor: '#e2e2e2',
+    borderTopRightRadius: 25,
+    borderTopLeftRadius: 25,
+    borderWidth: 1,
+    borderColor: '#e2e2e2',
+    color: '#000',
   },
   text: {
-    fontSize: 18,
+    fontSize: 16,
     fontWeight: 'bold',
-    textAlign: 'left',
-    marginBottom: 10,
   },
-  buttonText: {
-    fontSize: 18,
-    fontWeight: 'bold',
-    color: '#000',
-    padding: 10,
-    textAlign: 'center',
+  input: {
+    fontSize: 16,
+    marginTop: 15,
+    marginBottom: 15,
+    backgroundColor: '#fff',
+    height: 50,
+    paddingHorizontal: 10,
   },
-  modal: {},
 });
