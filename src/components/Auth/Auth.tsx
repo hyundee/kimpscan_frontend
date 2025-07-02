c4ab3bd3cf7049ab5196edeebcc194759d554785
a3magic3pocket <a3magic3pocket@gmail.com>
2025-07-02 10:24:18 +0900

    feat: 구글 로그인 시 fcm 키도 저장하도록 수정.

--- src/components/Auth/Auth.tsx
+++ src/components/Auth/Auth.tsx
@@ -1,7 +1,40 @@
-import { View } from "react-native"
+import { Alert, View } from "react-native"
 import { GoogleAuth } from "./GoogleAuth"
+import { URLS } from "@/constants/urls";
+import { useFcm } from "@/store/useFcm";
+import { useEffect } from "react";
+import { useLogin } from "@/store/useLogin";
+import authAxios from "@/lib/authAxios";
 
 export const Auth = () => {
+  const fcmKey = useFcm(state => state.fcmKey);
+  const hasJwt = useLogin(state => state.hasJwt)
+  const setHasJwt = useLogin(state => state.setHasJwt)
+  const setIsLoggedIn = useLogin(state => state.setIsLoggedIn)
+
+  const requestToAddFcmKey = async () => {
+    try {
+      const url = `${URLS.API_URL}/user/fcm`;
+      const data = { key: fcmKey };
+      await authAxios.post(url, data);
+      console.log('FCM 키 등록 성공');
+    } catch (error) {
+      Alert.alert('FCM 키 등록 실패', 'FCM 키 등록 실패.');
+      console.error('FCM 키 등록 실패', error)
+      setHasJwt(false)
+      throw error;
+    }
+  }
+
+  useEffect(() => {
+    if (hasJwt) {
+      requestToAddFcmKey();
+      setIsLoggedIn(true);
+    }
+
+  }, [hasJwt])
+
+
   return <View>
     <GoogleAuth />
   </View>
