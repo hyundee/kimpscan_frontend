0c1b379994ff5b3d39b46d1154b5ec3f18730031
a3magic3pocket <a3magic3pocket@gmail.com>
2025-07-02 10:23:03 +0900

    feat: 원하는 탭으로 navigate 하는 기능 구현.

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {StyleSheet} from 'react-native';
-import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
-import {HomeScreen} from '../screens/HomeScreen';
-import {MyPageScreen} from '../screens/MyPageScreen';
+import { createNavigationContainerRef, NavigationContainer } from '@react-navigation/native';
+import { StyleSheet } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { HomeScreen } from '../screens/HomeScreen';
+import { MyPageScreen } from '../screens/MyPageScreen';
 
-import {Header} from '../components/Header/Header';
+import { Header } from '../components/Header/Header';
 import Home from 'react-native-vector-icons/Foundation';
 import User from 'react-native-vector-icons/Feather';
+import { RootStackParamList } from '@/types/navigation';
+
+export const navigationRef = createNavigationContainerRef<RootStackParamList>();
+
+export function navigate(
+  ...args: Parameters<typeof navigationRef.navigate>
+) {
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(...args);
+  }
+}
 
 const Tab = createBottomTabNavigator();
 
 const CustomHeader = () => <Header />;
 
-const HomeTabIcon = ({color, size}: {color: string; size: number}) => (
+const HomeTabIcon = ({ color, size }: { color: string; size: number }) => (
   <Home name="home" size={size} color={color} style={styles.tabBarIcon} />
 );
 
-const MyPageTabIcon = ({color, size}: {color: string; size: number}) => (
+const MyPageTabIcon = ({ color, size }: { color: string; size: number }) => (
   <User name="user" size={size} color={color} style={styles.tabBarIcon} />
 );
 
 export const AppNavigator = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={navigationRef}>
       <Tab.Navigator
         screenOptions={{
           header: CustomHeader,
